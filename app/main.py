9bc6b08223db0540772c4ea20313a7d3f1979a95
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-10-26 23:12:54 +0300

    Removed movie endpoints from main.py

--- app/main.py
+++ app/main.py
@@ -9,29 +9,5 @@ async def read_root():
 # Authentication Routes
 
 
-# Movie Endpoints 
-# Get All Movies Route
-@app.get("/api/v1/movies")
-async def get_all_movies():
-    return {"message": "List of all movies"}
 
-# Get Movie by ID Route
-@app.get("/api/v1/movies/{id}")
-async def get_movie_by_id(id: int):
-    return {"message": f"Details of movie with id {id}"}
-
-# Search Movies Route
-@app.get("/api/v1/movies/search")
-async def search_movies(query: str):
-    return {"message": f"Search results for query: {query}"}
-
-# Get Movie Genres Route
-@app.get("/api/v1/movies/genres")
-async def get_movie_genres():
-    return {"message": "List of movie genres"}
-
-# Get Movies by Genre Route
-@app.get("/api/v1/movies/genre/{id}")
-async def get_movies():
-    return {"message": "List of movies"}
 
